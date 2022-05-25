af45aa0c100e29e65e36d02d9b84e0facf2092f8
JorgeOctavioAntunezP <jorge.oap18@gmail.com>
2022-05-25 11:18:55 -0700

    Program Numero Cuadrado
    Programa de excepciones al elevar un numero al cuadrado.

---
+++ Numero Cuadrado/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_Cuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alumno: Jorge Octavio Antunez Partida - 21211910
+
+            // Declaracion de Variables
+            int num;
+            bool rep;
+
+            do // Ciclo do-while asta no cometer errores
+            {
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("-Elevar un número al cuadrado-\n");
+                    // Asignar Valor a la Variable "num"
+                    Console.Write("   Número: ");
+                    num = int.Parse(Console.ReadLine());
+
+                    // Imprimir Resultado
+                    Console.WriteLine("\n   Resultado: " + Math.Pow(num, 2));
+                    rep = false;
+                }
+                // Excepciones
+                catch (FormatException)
+                {
+                    Console.WriteLine("\n!El dato tiene que ser un número entero¡");
+                    Console.ReadKey();
+                    rep = true;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("\n!El dato es demasiado grande¡");
+                    Console.ReadKey();
+                    rep = true;
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                    Console.ReadKey();
+                    rep = true;
+                }
+            }
+            while (rep == true);
+
+            // Salir del Programa
+            Console.Write("\nPreciona ENTER para salir. . .");
+            Console.ReadKey();
+        }
+    }
+}
